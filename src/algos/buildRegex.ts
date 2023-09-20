8099c1770bc4f4c55af11036b19bf9bf88e22178
Conlin Durbin <conlindurbin@protonmail.com>
2023-09-20 15:59:29 +0000

    Update license to Do No Harm license and fix regex

--- src/algos/buildRegex.ts
+++ src/algos/buildRegex.ts
@@ -1,6 +1,6 @@
 const buildRegex = (terms: string[]) => {
     const regexFields = terms.map(term => term.replace(/\s/gi, '\\s*')).join('|');
-    const regexString = `(\\b)?#?(${regexFields})(\\b)?`;
+    const regexString = `(?:^|\\s)#?(${regexFields})(?:$|\\s)`;
     return new RegExp(regexString);
   };
 export default buildRegex;
